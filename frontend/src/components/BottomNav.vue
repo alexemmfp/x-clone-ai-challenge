e2d0a03d26dbc195a603d91a54bb9a54a6134c59
Alex <ale95ran@gmail.com>
2026-06-23 19:03:33 -0300

    style(web): fix attribute linebreak in BottomNav per ESLint rule

--- frontend/src/components/BottomNav.vue
+++ frontend/src/components/BottomNav.vue
@@ -18,7 +18,8 @@
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
         <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6V11c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/>
       </svg>
-      <span v-if="notifs.unreadCount > 0"
+      <span
+v-if="notifs.unreadCount > 0"
         class="absolute top-1 right-1 bg-sky-500 text-white text-[10px] rounded-full min-w-[14px] h-3.5 flex items-center justify-center px-0.5 leading-none">
         {{ notifs.unreadCount > 9 ? '9+' : notifs.unreadCount }}
       </span>
